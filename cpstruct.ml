89b208b4d21671b030b0677d6166b7eb6cd17c69
Martin Bodin <martin.bodin@inria.fr>
2026-06-15 13:05:43 +0200

    First (untested) version.

--- cpstruct.ml
+++ cpstruct.ml
@@ -13,6 +13,14 @@ module List = struct
         | x :: l -> aux x l in
       aux x l
 
+  let remove_last = function
+    | [] -> invalid_arg "remove_last"
+    | x :: l ->
+      let rec aux previous acc = function
+        | [] -> List.rev acc
+        | x :: l -> aux x (previous :: acc) l in
+      aux x [] l
+
 end
 
 
@@ -24,14 +32,13 @@ let verbose = ref false
 
 let log fmt =
   if !verbose then Printf.ksprintf print_endline fmt
-  else Printf.isprintf fmt
+  else Printf.ifprintf () fmt
 
-let warning =
-  Printf.ksprintf (fun s -> prerr_endline ("🟧 Warning: " ^ s))
+let warning fmt =
+  Printf.ksprintf (fun s -> prerr_endline ("🟧 Warning: " ^ s)) fmt
 
-let error =
-  Printf.ksprintf (fun s -> prerr_endline ("🟥 Error: " ^ s)) ;
-  exit 1
+let error fmt =
+  Printf.ksprintf (fun s -> prerr_endline ("🟥 Error: " ^ s) ; exit 1) fmt
 
 let () =
   if not Sys.unix then warning "This program was designed and tested for Unix systems."
@@ -53,12 +60,15 @@ let () =
   check "source" !source_dir ;
   check "destination" !dest_dir ;
   let normalise dir =
-    if (!dir).(String.length !dir - 1) <> "/" then
+    if (!dir).[String.length !dir - 1] <> '/' then
       dir := !dir ^ "/" in
   normalise source_dir ;
   normalise dest_dir ;
   if !source_dir = !dest_dir then
     error "The source directory is identical to the destination directory."
+
+let source_dir = !source_dir
+let dest_dir = !dest_dir
 
 (* We represent a path as the list of directories followed by the filename. *)
 type path = string list
@@ -66,10 +76,8 @@ type path = string list
 let path_to_file ?from (p : path) =
   match from with
   | None ->
-    if p = [] then (
-      warning "Somehow getting the root directory during computation." ;
-      "/"
-    ) else String.concat "/" p
+    if p = [] then "/"
+    else String.concat "/" p
   | Some from ->
     if p = [] then from
     else (from ^ "/" ^ String.concat "/" p)
@@ -78,7 +86,7 @@ let path_to_file ?from (p : path) =
  For each file, we return the path (excluding the global diractory) as a list.
  An singleton path means that the file is directly in the [dir] directory. *)
 let get_all_files global_dir : path list =
-  log "Getting all files from %s." dir
+  log "Getting all files from %s." global_dir ;
   let rec aux prefix =
     log " Entering folder %s" (path_to_file ~from:global_dir prefix) ;
     let all_direct_files =
@@ -87,8 +95,8 @@ let get_all_files global_dir : path list =
       let path = prefix @ [file] in
       if Sys.is_directory (path_to_file ~from:global_dir path) then
         aux path
-      else path) (all_direct_files dir) in
-  prefix []
+      else [path]) all_direct_files in
+  aux []
 
 module SMap = Map.Make (String)
 
@@ -103,19 +111,56 @@ let build_listing dir =
         (path_to_file ~from:dir path) (path_to_file ~from:dir p') ;
       database) SMap.empty (get_all_files dir)
 
-let source_listing = build_listing !source_dir
-let dest_listing = build_listing !dest_dir
+let source_listing = build_listing source_dir
+let dest_listing = build_listing dest_dir
 
 let same_files path1 path2 =
   let file1 = path_to_file path1 in
   let file2 = path_to_file path2 in
   Digest.file file1 = Digest.file file2
 
+(* Ensures that the directory exists. *)
+let force_exists path =
+  let rec aux prefix = function
+    | [] -> ()
+    | x :: l ->
+      let prefix = prefix @ [x] in
+      let dir = path_to_file prefix in
+      if not (Sys.file_exists dir) then (
+        log "🟩 Creating directory %s." dir ;
+        Sys.mkdir dir 755
+      ) ;
+      aux prefix l in
+  aux [] path
+
+
+let command cmd args =
+  let cmd =
+    if args = [] then cmd
+    else (cmd ^ " " ^ String.concat " " args) in
+  (* log "🟦 Executing: %s" cmd ; *)
+  let r = Sys.command cmd in
+  if r < 0 then error "Command with negative result: %s" cmd
+
 let cp path1 path2 =
-  ()
+  force_exists (List.remove_last path2) ;
+  let source = path_to_file path1 in
+  let dest = path_to_file path2 in
+  if source <> dest then (
+    if Sys.file_exists dest then
+      warning "File would be replaced by copy: %s. Ignoring copy from %s." dest source
+    else command "cp" [source ; dest]
+  )
 
 let mv path1 path2 =
-  ()
+  force_exists (List.remove_last path2) ;
+  let source = path_to_file path1 in
+  let dest = path_to_file path2 in
+  if source <> dest then (
+    if Sys.file_exists dest then
+      warning "File would be replaced by copy: %s. Ignoring copy from %s." dest source
+    else Sys.rename (path_to_file path1) (path_to_file path2)
+  )
 
 let () =
   SMap.iter (fun file path ->
